6e5992afb71c9fdd7feb78ce47912e1ef045f063
Dan <71558240+Exploit-0@users.noreply.github.com>
2020-10-25 14:56:50 +0530

    T1

--- app/src/main/java/com/zoop/video/MainActivity.java
+++ app/src/main/java/com/zoop/video/MainActivity.java
@@ -1,14 +1,33 @@
 package com.zoop.video;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar_home);
+        setSupportActionBar(toolbar);
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_home_menu, menu);
+        return true;
     }
 }
